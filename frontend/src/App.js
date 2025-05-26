304329b4ed6bccb88b8611590242d2ebb41ee5ea
Shubham-devs <snpjogi123@gmail.com>
2025-05-26 21:56:34 +0530

    first

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+// import logo from './logo.svg';
+// import { Button } from '@chakra-ui/react';
+import './App.css';
+import { Route } from 'react-router-dom/cjs/react-router-dom.min';
+import Homepage from './Pages/Homepage';
+import Chatpage from './Pages/Chatpage';
+
+function App() {
+  return (
+    <div className='App'>
+
+      <Route path='/' component={Homepage} exact />
+      <Route path='/chats' component={Chatpage} />
+
+    </div>
+  );
+}
+
+export default App;
